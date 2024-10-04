5345805e6dcedd1e2d60ab2ac4dd4df760be7633
kozhydlo <markkozhydlo@gmail.com>
2024-10-04 14:10:47 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
+// import React, { useState } from 'react';
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import AddFriend from './components/AddFriend';
+// import FriendsList from './components/FriendsList';
+//
+// const App = () => {
+//     const [token, setToken] = useState(null);
+//     const [isLogin, setIsLogin] = useState(true); // Контролюємо перемикання між входом і реєстрацією
+//
+//     const handleSignupSuccess = (token) => {
+//         setToken(token);
+//     };
+//
+//     const handleLoginSuccess = (token) => {
+//         setToken(token);
+//     };
+//
+//     const toggleForm = () => {
+//         setIsLogin(!isLogin); // Перемикання між Login та Signup
+//     };
+//
+//     return (
+//         <div className="App">
+//             {!token ? (
+//                 <>
+//                     {isLogin ? (
+//                         <Login onLoginSuccess={handleLoginSuccess} />
+//                     ) : (
+//                         <Signup onSignupSuccess={handleSignupSuccess} />
+//                     )}
+//
+//                 </>
+//             ) : (
+//                 <FriendsList token={token} />
+//                 // <div className="flex justify-center space-x-4">
+//                 //     {/*<AddFriend token={token} onFriendAdded={() => {}} />*/}
+//                 //     <FriendsList token={token} />
+//                 // </div>
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default App;
+
 import React, { useState } from 'react';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import AddFriend from './components/AddFriend';
+import AuthPage from './components/AuthPage'; // Замість окремого імпорту Login і Signup
 import FriendsList from './components/FriendsList';
 
 const App = () => {
-    const [token, setToken] = useState(null);
+    const [token, setToken] = useState(null); // Зберігаємо токен
     const [isLogin, setIsLogin] = useState(true); // Контролюємо перемикання між входом і реєстрацією
 
     const handleSignupSuccess = (token) => {
@@ -23,20 +67,12 @@ const App = () => {
     return (
         <div className="App">
             {!token ? (
-                <>
-                    {isLogin ? (
-                        <Login onLoginSuccess={handleLoginSuccess} />
-                    ) : (
-                        <Signup onSignupSuccess={handleSignupSuccess} />
-                    )}
-
-                </>
+                <AuthPage
+                    onLoginSuccess={handleLoginSuccess}
+                    onSignupSuccess={handleSignupSuccess}
+                />
             ) : (
                 <FriendsList token={token} />
-                // <div className="flex justify-center space-x-4">
-                //     {/*<AddFriend token={token} onFriendAdded={() => {}} />*/}
-                //     <FriendsList token={token} />
-                // </div>
             )}
         </div>
     );
--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -136,3 +136,4 @@ const FriendsList = ({ token }) => {
 };
 
 export default FriendsList;
+
